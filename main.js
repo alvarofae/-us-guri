966f3745dc860a5cc7efe118035374ce33e53f35
alvarofae <105504676+alvarofae@users.noreply.github.com>
2024-04-09 10:38:28 -0300

    feito

--- main.js
+++ main.js
@@ -11,3 +11,5 @@ for (let i = 0; i<botoes.length; i++) {
         textos[i].classList.add("ativo");
     }
 }
+const contadores = documentquerysellectorAll(".contador");
+contadores[0].textContent = "contagem regressiva"

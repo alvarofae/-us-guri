14d23b870ef3063f65d6306ac8ca6e8754d347d6
alvarofae <105504676+alvarofae@users.noreply.github.com>
2024-05-10 08:57:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i<botoes.length; i++) {
-    botoes [i].onclick = function(){
-        for(let j=0; j<botoes.length; j++){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -12,12 +11,24 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-04-19T23:59:59");
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
 function calculaTempo(tempoObjetivo1){
     let tempoAtual = new Date();
-    contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+    let tempoFinal = tempoObjetivo1 - tempoAtual;
     let segundos = Math.floor(tempoFinal / 1000);
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+return dias + "dias"
+    + horas + "hora"
+    + minutos + "minutos"
+    + segundos + "segundos";
 }
